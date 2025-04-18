3d9143ecaef58d04f7106b5436f9d4d5d3ccb3a3
VaynerAkaWalo <vaynerakawalo@gmail.com>
2025-04-18 23:30:40 +0200

    Zadanie 3 Reagowanie na wiadomosci uzytkownikow

--- zadanie_03/src/main/kotlin/DiscordBot.kt
+++ zadanie_03/src/main/kotlin/DiscordBot.kt
@@ -3,13 +3,17 @@ package ebiznes
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Activity
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import net.dv8tion.jda.api.hooks.EventListener
+import net.dv8tion.jda.api.hooks.ListenerAdapter
 
-class DiscordBot(private var token: String) {
+class DiscordBot(private var token: String) : ListenerAdapter() {
     private lateinit var bot: JDA
 
     fun startBot() {
         bot = JDABuilder.createDefault(token)
             .setActivity(Activity.playing("Ebiznes"))
+            .addEventListeners(this)
             .build()
             .awaitReady()
     }
@@ -18,6 +22,17 @@ class DiscordBot(private var token: String) {
         val chanels = bot.getTextChannelsByName("ogólny", true)
         for(chan in chanels) {
             chan.sendMessage("Hello world").queue()
+        }
+    }
+
+    override fun onMessageReceived(event: MessageReceivedEvent) {
+        if (event.message.author.isBot) {
+            return
+        }
+        event.message.mentions
+
+        if (event.message.mentions.isMentioned(bot.selfUser)) {
+            event.channel.sendMessage("Odebralem wiadomosc " + event.message.contentRaw).queue()
         }
     }
 }
